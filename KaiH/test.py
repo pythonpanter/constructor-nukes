3beda0b86994715a7446469ce0d7b7d907243256
data_kai <kai.hugenroth@rwth-aachen.de>
2022-11-30 15:55:00 +0100

    test env succ

--- KaiH/test.py
+++ KaiH/test.py
@@ -1,6 +1,17 @@
+import sys
+usr=['jvk','ak','kb','kh']
+dirs=['Jonas','Alexej','KaiB']
+for d in dirs:
+    sys.path.insert(0, f'../{d}')
+
+import importjvk as jvk
+#import importak as ak              #Alexej not yet compatible
+
+
+
+#testdata
 import inputload as il
 import plotter as cp
-
 import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -11,12 +22,22 @@ import json
 from copy import deepcopy
 
 
+
+
+
+lj=jvk.get_data()
+
+print(type(lj[0]))
+print(type(lj[1]))
+
+
+
 print('start')
 st.title('This is a title ...')
 il.test()
 
-k,df=il.get_data()
 
+k,df=il.get_data()
 
 print(k)
 print(df.head())
@@ -28,6 +49,10 @@ print(info_dict)
 
 st.plotly_chart(fig)
 st.plotly_chart(fig)
+st.plotly_chart(fig)
+st.plotly_chart(fig)
+st.plotly_chart(fig)
+
 
 print('end')
 
