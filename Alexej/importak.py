11566cd1f13bbf574725cdf3ac42930765932f3e
Alexej Khalilzada <alexej.khalilzada@googlemail.com>
2022-11-30 15:51:32 +0100

    add function with return value

--- Alexej/importak.py
+++ Alexej/importak.py
@@ -9,26 +9,37 @@
 import pandas as pd
 
 
-##### Read in the data
-# nuclear_weapons_proliferation_owid.csv
-df_proliferation = pd.read_csv('/Users/alexejkhalilzada/Dokumente/Data_Science/SIT_Learning/GroupChallenge/constructor-nukes/dataset/nuclear_weapons_proliferation_owid.csv')
-# df_proliferation = pd.read_csv('nuclear_weapons_proliferation_owid.csv', index_col=0)
-#df_proliferation = data.sort_values(by=['country_name', 'year'])
-#df_proliferation = data.reset_index()
 
-# nuclear_weapons_stockpiles.csv
-df_stockpiles = pd.read_csv('/Users/alexejkhalilzada/Dokumente/Data_Science/SIT_Learning/GroupChallenge/constructor-nukes/dataset/nuclear_weapons_stockpiles.csv')
+def get_data():
 
-# nuclear_weapons_tests_states.csv
-df_states = pd.read_csv('/Users/alexejkhalilzada/Dokumente/Data_Science/SIT_Learning/GroupChallenge/constructor-nukes/dataset/nuclear_weapons_tests_states.csv')
+    pd.set_option('display.max_rows', 500)
+    pd.set_option('display.max_columns', 500)
+    pd.set_option('display.width', 1000)
+    
+    
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+    
+    urlcsv=dir_path+"/../dataset/nuclear_weapons_stockpiles.csv"
+    stockpiles = pd.read_csv(urlcsv, index_col = [0,1], skipinitialspace=True).reset_index()
+    
+    urlcsv=dir_path+"/../dataset/nuclear_weapons_tests_states.csv"
+    tests_states = pd.read_csv(urlcsv, index_col = [0,1], skipinitialspace=True).reset_index()
+    
+    urlcsv=dir_path+"/../dataset/nuclear_weapons_proliferation_total_owid.csv"
+    proliferationTot = pd.read_csv(urlcsv, index_col = [0,1], skipinitialspace=True)
+    
+    urlcsv=dir_path+"/../dataset/nuclear_weapons_proliferation_owid.csv"
+    proliferation = pd.read_csv(urlcsv, index_col = [0,1], skipinitialspace=True).reset_index()
 
-###### Display dataframes
-print('* df_proliferation = \n', df_proliferation)
-print('* df_stockpiles = \n', df_stockpiles)
-print('* df_states = \n', df_states)
 
-##### Merge dataframes
-df_merged = pd.merge(pd.merge(df_proliferation,df_stockpiles,on='country_name'),df_states,on='country_name')
-df_merged_2 = pd.merge(pd.merge(df_proliferation,df_stockpiles,on=['country_name', 'year']),df_states,on=['country_name', 'year'])
-print('* df_merged = \n', df_merged.head(60))
-print('* df_merged_2 = \n', df_merged_2.head(60))
+    ##### Read in the data
+    df_proliferation = pd.read_csv('/Users/alexejkhalilzada/Dokumente/Data_Science/SIT_Learning/GroupChallenge/constructor-nukes/dataset/nuclear_weapons_proliferation_owid.csv')
+    # nuclear_weapons_stockpiles.csv
+    df_stockpiles = pd.read_csv('/Users/alexejkhalilzada/Dokumente/Data_Science/SIT_Learning/GroupChallenge/constructor-nukes/dataset/nuclear_weapons_stockpiles.csv')
+    # nuclear_weapons_tests_states.csv
+    df_states = pd.read_csv('/Users/alexejkhalilzada/Dokumente/Data_Science/SIT_Learning/GroupChallenge/constructor-nukes/dataset/nuclear_weapons_tests_states.csv')
+
+    ##### Merge dataframes
+    df_merged = pd.merge(pd.merge(df_proliferation,df_stockpiles,on=['country_name', 'year']),df_states,on=['country_name', 'year'])
+    
+    return "Alexej1", df_merged
