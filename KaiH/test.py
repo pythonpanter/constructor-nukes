27c96273ded086cec9381862e8561626ce9b74cd
data_kai <kai.hugenroth@rwth-aachen.de>
2022-12-01 09:45:33 +0100

    update

--- KaiH/test.py
+++ KaiH/test.py
@@ -9,69 +9,30 @@ dirs=['Jonas','Alexej','KaiB']
 
 for d in dirs:
     sys.path.insert(0, f'{dots}/{d}')
+import importjvk as jvk
+import importak as ak
+dataframes=[]
+dataframes.append(jvk.get_data())
+dataframes.append(ak.get_data())
+n,df=dataframes[0]
+import pandas as pd
+print(df.head())
+
+
+
 
 
 
 import importjvk as jvk
+
 import importak as ak
-
-
-
-#testdata
-import inputload as il
-import plotter as cp
-import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
-
 import plotly.express as px
+
 import plotly.graph_objects as go
 from urllib.request import urlopen
 import json
 from copy import deepcopy
-"""
-import plotkb as pkb
 
-plots=pkb.get_plots()
-(key,fig,info_dict)=plots[0]
-st.title('Kai B Plot')
-st.plotly_chart(fig)
-#(key,fig,info_dict)=pkb.cplot(df)
-"""
-
-lj=jvk.get_data()
-
-print(type(lj[0]))
-print(type(lj[1]))
-
-
-ak=ak.get_data()
-
-print(type(ak[0]))
-print(type(ak[1]))
-
-print('start')
-st.title('This is a title ...')
-il.test()
-
-
-
-k,df=il.get_data()
-
-print(k)
-print(df.head())
-
-(key,fig,info_dict)=cp.cplot(df)
-print(key)
-print(info_dict)
-
-
-st.plotly_chart(fig)
-st.plotly_chart(fig)
-st.plotly_chart(fig)
-st.plotly_chart(fig)
-st.plotly_chart(fig)
-
-
-print('end')
-
+#st.title('Nukes')
