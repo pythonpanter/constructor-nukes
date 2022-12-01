cfc9fd03e13292114c3a71052c9d4d9a1e1439b4
data_kai <kai.hugenroth@rwth-aachen.de>
2022-12-01 13:20:07 +0100

    update

--- KaiH/test.py
+++ KaiH/test.py
@@ -10,18 +10,7 @@ else:
 dirs=['Jonas','Alexej','KaiB']
 for d in dirs:
     sys.path.insert(0, f'{dots}/{d}')
-
-
-
-
-# IMPORT ADDITIONAL PACKAGES
-from urllib.request import urlopen
-import plotly.graph_objects as go
-import plotly.express as px
-import matplotlib.pyplot as plt
-from copy import deepcopy
-import pandas as pd
-import json
+import streamlit as st
 
 # SET USR
 usr= {'jvk':0,
@@ -29,47 +18,45 @@ usr= {'jvk':0,
       'kb':1,
       'kh':0}
 
-
-# SET STREAMLIT BASE
-import streamlit as st
-
+# STREAMLIT SIDEBAR
 st.sidebar.image(f'{kaipath}nuke.jpg')
 st.sidebar.title('NU*ES')
-slide = st.sidebar.radio('',['Intro','Jonas Story','Alexejs  Story','Kais  Story','Architecture'])
+slide = st.sidebar.radio('',['Intro','Jonas Story','Alexejs  Story','Kai Bs Story','Architecture'])
 st.header(slide)
 if sys.platform=='win32':
     st.sidebar.title('***LOCAL INSTANCE***')
+
+# STREAMLIT MAINPAGE
+#Helper
 def import_plots(plots):
     for i in range (0,len(plots)):
-        st.header(plots[i][2]['title'])
-        st.subheader(plots[i][2]['description'])
-        if (plots[i][2]['lib']=='plotly_go') or (plots[i][2]['lib']=='plotly_chart'):
-            st.plotly_chart(plots[i][1])
+         show_plot(plots,i)
 
-# IMPORT PLOTS
-if slide==('Kai Bs  Story'):
+def show_plot(plots,index):
+    st.header(plots[index][2]['title'])
+    st.subheader(plots[index][2]['description'])
+    if (plots[index][2]['lib'] == 'plotly_go') or (plots[index][2]['lib'] == 'plotly_chart'):
+        st.plotly_chart(plots[index][1])
+
+#Pagegen
+if slide==('Kai Bs Story'):
     if usr['kb']:
         import plotkb as pkb
         plots=pkb.get_plots()
         import_plots(plots)
-elif slide == ('kb'):
+elif slide == ('Alexejs  Story'):
     if usr['ak']:
         import plotak as pak
         plots=pak.get_plots()
         import_plots(plots)
-elif slide == ('kb'):
+elif slide == ('Jonas Story'):
     if usr['jvk']:
         import plotjvk as pjvk
         plots=pjvk.get_plots()
         import_plots(plots)
 elif slide == ('Architecture'):
-    #if usr['kh']:
-     #   import plotkh as pkh
-      #  plots=pkh.get_plots()
-       # import_plots(plots)
     st.image(f'{kaipath}architecture.jpg')
-    st.caption(
-        'Architecture of underlying framework (WIP)')
+    st.caption('Architecture of underlying framework for nu*de data analysis (WIP)')
 elif slide == ('Intro'):
     st.image(f'{kaipath}bikinibomb.jpg')
     st.caption('Showgirl Joy Healy smiles as she straddles a U.S. Air Force missile, wearing a bikini costume, at an American Federation of Labor Union show, in Los Angeles, California, in 1945  (Photo credit: Hulton Archive/Getty Images)')
