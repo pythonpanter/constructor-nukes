8350e3dcf2e1e75e246b4c458c11bc1544185f9d
data_kai <kai.hugenroth@rwth-aachen.de>
2022-12-01 13:39:41 +0100

    update

--- KaiH/test.py
+++ KaiH/test.py
@@ -18,10 +18,16 @@ usr= {'jvk':1,
       'kb':1,
       'kh':1}
 
+s=dict(Intro='Intro',
+            jonas='Jonas Story',
+            alexej='Alexejs  Story',
+            kaib='Kai Bs Story',
+            Architecture='Architecture')
+
 # STREAMLIT SIDEBAR
 st.sidebar.image(f'{kaipath}nuke.jpg')
 st.sidebar.title('NU*ES')
-slide = st.sidebar.radio('',['Intro','Jonas Story','Alexejs  Story','Kai Bs Story','Architecture'])
+slide = st.sidebar.radio('',[s['Intro'],s['jonas'],s['alexej'],s['kaib'],s['Architecture']])
 st.header(slide)
 if sys.platform=='win32':
     st.sidebar.title('***LOCAL INSTANCE***')
@@ -39,24 +45,24 @@ def show_plot(plots,index):
         st.plotly_chart(plots[index][1])
 
 #Pagegen
-if slide==('Kai Bs Story'):
+if slide==(s['kaib']):
     if usr['kb']:
         import plotkb as pkb
         plots=pkb.get_plots()
         import_plots(plots)
-elif slide == ('Alexejs  Story'):
+elif slide == (s['alexej']):
     if usr['ak']:
         import plotak as pak
         plots=pak.get_plots()
         import_plots(plots)
-elif slide == ('Jonas Story'):
+elif slide == (s['jonas']):
     if usr['jvk']:
         import plotjvk as pjvk
         plots=pjvk.get_plots()
         import_plots(plots)
-elif slide == ('Architecture'):
+elif slide == (s['Architecture']):
     st.image(f'{kaipath}architecture.jpg')
     st.caption('Architecture of underlying framework for nu*de data analysis (WIP)')
-elif slide == ('Intro'):
+elif slide == (s['Intro']):
     st.image(f'{kaipath}bikinibomb.jpg')
     st.caption('Showgirl Joy Healy smiles as she straddles a U.S. Air Force missile, wearing a bikini costume, at an American Federation of Labor Union show, in Los Angeles, California, in 1945  (Photo credit: Hulton Archive/Getty Images)')
