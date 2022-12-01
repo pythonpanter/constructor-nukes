1d00a6546d041ae876fe86189dc0a31d72f912d2
data_kai <kai.hugenroth@rwth-aachen.de>
2022-12-01 10:00:33 +0100

    update

--- KaiH/test.py
+++ KaiH/test.py
@@ -1,46 +1,41 @@
+# SET FOLDER ENVIRONMENT
 import sys
 if sys.platform=='win32':
     dots='..'
 else:
     dots='.'
-
 usr=['jvk','ak','kb','kh']
 dirs=['Jonas','Alexej','KaiB']
-
 for d in dirs:
     sys.path.insert(0, f'{dots}/{d}')
-import importjvk as jvk
-import importak as ak
-dataframes=[]
-dataframes.append(jvk.get_data())
-dataframes.append(ak.get_data())
-n,df=dataframes[0]
-import pandas as pd
-print(df.head())
+
+# SET STREAMLIT BASE
 import streamlit as st
+if sys.platform=='win32':
+    st.title('***LOCAL INSTANCE***')
 st.title('Nukes')
+
+
+# IMPORT ADDITIONAL PACKAGES
+from urllib.request import urlopen
+import plotly.graph_objects as go
+import plotly.express as px
+import matplotlib.pyplot as plt
+from copy import deepcopy
+import pandas as pd
+import json
+
+# IMPORT PLOTS
+
 import plotkb as pkb
 
 plots=pkb.get_plots()
 (key,fig,info_dict)=plots[0]
+
 st.title('Kai B Plot')
 st.plotly_chart(fig)
 
 
 
 
-
-
-
-import importjvk as jvk
-
-import importak as ak
-import pandas as pd
-import matplotlib.pyplot as plt
-import plotly.express as px
-
-import plotly.graph_objects as go
-from urllib.request import urlopen
-import json
-from copy import deepcopy
 
