baa3da8f54349089ec0728aad60b4732beaccc9f
data_kai <kai.hugenroth@rwth-aachen.de>
2022-12-01 11:18:09 +0100

    update

--- KaiH/test.py
+++ KaiH/test.py
@@ -14,10 +14,7 @@ for d in dirs:
 # SET STREAMLIT BASE
 import streamlit as st
 if sys.platform=='win32':
-    st.title('***LOCAL INSTANCE***')
-st.title('Nuke. N.')
-st.image(f'{kaipath}bikinibomb.jpg')
-st.caption('Showgirl Joy Healy smiles as she straddles a U.S. Air Force missile, wearing a bikini costume, at an American Federation of Labor Union show, in Los Angeles, California, in 1945  (Photo credit: Hulton Archive/Getty Images)')
+    st.sidebar.title('***LOCAL INSTANCE***')
 
 # IMPORT ADDITIONAL PACKAGES
 from urllib.request import urlopen
@@ -30,10 +27,15 @@ import json
 
 # IMPORT PLOTS
 
+
+
 usr= {'jvk':0,
       'ak':0,
       'kb':1,
       'kh':0}
+
+slide = st.sidebar.radio('Slide',['nu*es','Jonas','Alexej','Kai B','Kai H'])
+st.header(slide)
 
 def import_plots(plots):
     for i in range (0,len(plots)):
@@ -42,33 +44,27 @@ def import_plots(plots):
         if (plots[i][2]['lib']=='plotly_go') or (plots[i][2]['lib']=='plotly_chart'):
             st.plotly_chart(plots[i][1])
 
-if usr['kb']:
-    import plotkb as pkb
-    plots=pkb.get_plots()
-    st.header('Kai B Plots')
-    import_plots(plots)
 
-if usr['ak']:
-    import plotak as pak
-    plots=pak.get_plots()
-    st.header('Alexej Plots')
-    import_plots(plots)
-
-if usr['jvk']:
-    import plotjvk as pjvk
-    plots=pjvk.get_plots()
-    st.header('Jonas Plots')
-    import_plots(plots)
-
-if usr['kh']:
-    import plotkh as pkh
-    plots=pkh.get_plots()
-    st.header('Kai H Plots')
-    import_plots(plots)
-
-
-
-
-
-
-
+if slide==('kb'):
+    if usr['kb']:
+        import plotkb as pkb
+        plots=pkb.get_plots()
+        import_plots(plots)
+elif slide == ('kb'):
+    if usr['ak']:
+        import plotak as pak
+        plots=pak.get_plots()
+        import_plots(plots)
+elif slide == ('kb'):
+    if usr['jvk']:
+        import plotjvk as pjvk
+        plots=pjvk.get_plots()
+        import_plots(plots)
+elif slide == ('kb'):
+    if usr['kh']:
+        import plotkh as pkh
+        plots=pkh.get_plots()
+        import_plots(plots)
+elif slide == ('nu*es'):
+    st.image(f'{kaipath}bikinibomb.jpg')
+    st.caption('Showgirl Joy Healy smiles as she straddles a U.S. Air Force missile, wearing a bikini costume, at an American Federation of Labor Union show, in Los Angeles, California, in 1945  (Photo credit: Hulton Archive/Getty Images)')
