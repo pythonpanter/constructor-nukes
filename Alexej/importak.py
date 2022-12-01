7de0e8738686b429c693b11c3e16c4b41027cdbe
Alexej Khalilzada <alexej.khalilzada@googlemail.com>
2022-12-01 13:26:40 +0100

    pimp my code

--- Alexej/importak.py
+++ Alexej/importak.py
@@ -40,18 +40,6 @@ def get_data():
     print(df_stockpiles)
     print(df_tests)
     print(df_proliferation)
-    
-    # urlcsv = dir_path+"/../dataset/nuclear_weapons_stockpiles.csv"
-    # df_stockpiles = pd.read_csv(
-    #     urlcsv, index_col=[0, 1], skipinitialspace=True).reset_index()
-
-    # urlcsv = dir_path+"/../dataset/nuclear_weapons_tests_states.csv"
-    # df_states = pd.read_csv(
-    #     urlcsv, index_col=[0, 1], skipinitialspace=True).reset_index()
-
-    # urlcsv = dir_path+"/../dataset/nuclear_weapons_proliferation_owid.csv"
-    # df_proliferation = pd.read_csv(
-    #     urlcsv, index_col=[0, 1], skipinitialspace=True).reset_index()
 
     # Merge dataframes
     df_merged = pd.merge(pd.merge(df_proliferation, df_stockpiles, on=[
@@ -84,53 +72,62 @@ print('* merged_df = .......\n', merged_df)
 print('* Len of dataframe "merged_df" = ', len(merged_df))
 for column in merged_df:
     # print('* Column in dataframe = ', merged_df[column])
-    print(column)
-
-alpha = 0.05 # p value
-
-##### Assumption Check
-# H₀: The data is normally distributed.
-# H₁: The data is not normally distributed.
-# α=0.05. If the p-value is >0.05, it can be said that data is normally distributed.
-
-# check whether data is normally distributed
-# For checking normality, I used Shapiro-Wilk’s W test which is generally preferred for smaller samples however 
-# there are other options like Kolmogorov-Smirnov and D’Agostino and Pearson’s test. 
-# Please visit https://docs.scipy.org/doc/scipy/reference/stats.html for more information.
-def check_normal_distribution(data):
-    test_stat_normality, p_value_normality=stats.shapiro(data)
-    print("p value:%.4f" % p_value_normality)
-    if p_value_normality <0.05:
-        print("Reject null hypothesis >> The data is not normally distributed")
+    if column == 'country_name':
+        pass
     else:
-        print("Fail to reject null hypothesis >> The data is normally distributed")
+        print('* Column in dataframe = ', merged_df[column])
+        print(column)
+        print(merged_df[column])
 
-check_normal_distribution(sunny_days_san_francisco)
-check_normal_distribution(sunny_day_boston)
+    alpha = 0.05 # p value
 
-##### Function
-# Levene's test
-# For checking variance homogeneity, I preferred Levene’s test but you can also check Bartlett’s test 
-# from here: https://docs.scipy.org/doc/scipy/reference/generated/scipy.stats.bartlett.html#scipy.stats.bartlett
-def check_variance_homogeneity(sunny_days_san_francisco, sunny_day_boston):
-    test_stat_var, p_value_var= stats.levene(sunny_days_san_francisco,sunny_day_boston)
-    print("p value:%.4f" % p_value_var)
-    if p_value_var <0.05:
-        print("Reject null hypothesis >> The variances of the samples are different.")
+    ##### Assumption Check
+    # H₀: The data is normally distributed.
+    # H₁: The data is not normally distributed.
+    # α=0.05. If the p-value is >0.05, it can be said that data is normally distributed.
+
+    # check whether data is normally distributed
+    # For checking normality, I used Shapiro-Wilk’s W test which is generally preferred for smaller samples however 
+    # there are other options like Kolmogorov-Smirnov and D’Agostino and Pearson’s test. 
+    # Please visit https://docs.scipy.org/doc/scipy/reference/stats.html for more information.
+    def check_normal_distribution(data):
+        test_stat_normality, p_value_normality=stats.shapiro(data)
+        print("p value:%.4f" % p_value_normality)
+        if p_value_normality <0.05:
+            print("Reject null hypothesis >> The data is not normally distributed")
+        else:
+            print("Fail to reject null hypothesis >> The data is normally distributed")
+            
+    print(f'* Checking normal distribution for column {column}')
+    
+    check_normal_distribution(merged_df[column])
+
+    # check_normal_distribution(sunny_days_san_francisco)
+    # check_normal_distribution(sunny_day_boston)
+
+    ##### Function
+    # Levene's test
+    # For checking variance homogeneity, I preferred Levene’s test but you can also check Bartlett’s test 
+    # from here: https://docs.scipy.org/doc/scipy/reference/generated/scipy.stats.bartlett.html#scipy.stats.bartlett
+    def check_variance_homogeneity(sunny_days_san_francisco, sunny_day_boston):
+        test_stat_var, p_value_var= stats.levene(sunny_days_san_francisco,sunny_day_boston)
+        print("p value:%.4f" % p_value_var)
+        if p_value_var <0.05:
+            print("Reject null hypothesis >> The variances of the samples are different.")
+        else:
+            print("Fail to reject null hypothesis >> The variances of the samples are same.")
+
+    # check_variance_homogeneity(sunny_days_san_francisco, sunny_day_boston)
+
+    ##### Since assumptions are satisfied, we can perform the parametric version of the test for 2 groups
+
+    ##### Calculate t-score and test the hypothesis with alpha=0.05 Assume that all variables are normally distributed. Do not make any assumptions about the variance.
+    #
+    # ttest,p_value = stats.ttest_ind(sunny_days_san_francisco,sunny_day_boston)
+    print("p value:%.8f" % p_value)
+    print("since the hypothesis is one sided >> use p_value/2 >> p_value_one_sided:%.4f" %(p_value/2))
+    if p_value/2 <0.05:
+        print("\nDo: --->  Reject null hypothesis")
     else:
-        print("Fail to reject null hypothesis >> The variances of the samples are same.")
-
-# check_variance_homogeneity(sunny_days_san_francisco, sunny_day_boston)
-
-##### Since assumptions are satisfied, we can perform the parametric version of the test for 2 groups
-
-##### Calculate t-score and test the hypothesis with alpha=0.05 Assume that all variables are normally distributed. Do not make any assumptions about the variance.
-#
-# ttest,p_value = stats.ttest_ind(sunny_days_san_francisco,sunny_day_boston)
-print("p value:%.8f" % p_value)
-print("since the hypothesis is one sided >> use p_value/2 >> p_value_one_sided:%.4f" %(p_value/2))
-if p_value/2 <0.05:
-    print("\nDo: --->  Reject null hypothesis")
-else:
-    print("\nDo: ---> Fail to reject null hypothesis")
+        print("\nDo: ---> Fail to reject null hypothesis")
 
