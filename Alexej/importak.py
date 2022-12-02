2b403e13fb3f5b64cb5993758b427a64ead0b3d8
Alexej Khalilzada <alexej.khalilzada@googlemail.com>
2022-12-02 12:19:53 +0100

    almost at the finish line

--- Alexej/importak.py
+++ Alexej/importak.py
@@ -9,8 +9,8 @@
 import pandas as pd
 import os
 import streamlit as st
-from scipy import stats
-import itertools
+import plotly.graph_objects as go
+import plotly.express as px
 
 ##### Variables
 list_of_columns = [] # List of columns
@@ -55,6 +55,69 @@ author = merged_df[0]
 merged_df = merged_df[1]
 
 # Debug Output
-print(f'* merged_df = {merged_df}')
+print(f'* merged_df = \n{merged_df}')
 print(f'* Length of dataframe "merged_df" = {len(merged_df)}')
 print(f'* Author = {author}')
+
+##### Playing around
+### Group by country
+# 
+dummy_df = merged_df.copy()
+overall_weapons_stockpile_per_country = dummy_df.groupby(['country_name', 'year'], as_index=False)['nuclear_weapons_stockpile'].sum()
+overall_weapons_tests_per_country = dummy_df.groupby(['country_name', 'year'], as_index=False)['nuclear_weapons_tests'].count()
+overall_weapons_status_per_country = dummy_df.groupby(['country_name', 'year'], as_index=False)['nuclear_weapons_status'].count()
+
+print('\n\n****************************************************************************************************************\n')
+print(overall_weapons_stockpile_per_country.head(60))
+print('\n\n****************************************************************************************************************\n')
+print(overall_weapons_tests_per_country.head(60))
+print('\n\n****************************************************************************************************************\n')
+print(overall_weapons_status_per_country.tail(60))
+
+# Merge dataframes
+overall_stockpiles_tests_merged_df = pd.merge(overall_weapons_stockpile_per_country, overall_weapons_tests_per_country, on=['country_name', 'year'])
+print('\n\n****************************************************************************************************************')
+print('****************************************************************************************************************\n')
+print(overall_stockpiles_tests_merged_df.tail(60))
+
+def dummy(dataframe):
+    ##### Plot
+    # 'year' in the x-axis and 'nuclear_weapons_stockpile' in the y-axis.
+    fig = px.line(dataframe, x="year", y="nuclear_weapons_stockpile", color="country_name")
+
+    # Marker's shape should be square.
+    fig.update_traces(
+        marker=dict(symbol="circle", line=dict(width=2, color="DarkSlateGrey")),
+        selector=dict(mode="markers"),
+    )
+
+    # Change color background for the chart and the whole plot.
+    fig.update_layout({
+        'plot_bgcolor': 'rgba(255, 255, 255, 255)',
+        'paper_bgcolor': 'rgba(255, 255, 255, 255)'
+    })
+
+    # Add title, and x and y labels with font size 18.
+    fig.update_xaxes(title_text='Year', title_font = {"size": 18})
+    fig.update_yaxes(title_text='Nuclear Weapons Stockpile', title_font = {"size": 18})
+    fig.update_layout(title_text="Nuclear Weapons Stockpile per Country per Year", title_yanchor = "top", legend_itemsizing="constant")
+
+    # return figure
+    return fig
+
+# weapons_tests_df = dummy_df.groupby(['country_name', 'year'], as_index=False)['nuclear_weapons_stockpile'].get_value()
+# print('\n\n*************************************************************************** Weapons Tests DF *****************************\n')
+# print(weapons_tests_df.tail(60))
+
+def get_plots():
+    title='Nuclear Weapons Stockpile per Country per Year'
+    description = 'This chart shows the nuclear weapons stockpile per country per year'
+    key='nuclear'
+    lib = 'plotly_express'
+    info_dict=dict(title=title, description=description, lib=lib)
+    
+    dummy(overall_stockpiles_tests_merged_df).show()
+    
+    return [(key, fig, info_dict)]
+
+get_plots()
