0357fd9892c24515a2ff7b8df571a7581016bb08
Alexej Khalilzada <alexej.khalilzada@googlemail.com>
2022-12-02 16:59:07 +0100

    fix annotations

--- Alexej/plotak.py
+++ Alexej/plotak.py
@@ -82,7 +82,7 @@ def dummy(dataframe):
     # fig.add_vline(x=1982, line_width=3, line_dash="dash", line_color="green", annotation_text="ThisIsAtestText")
     fig.add_vrect(x0=1947, x1=1991, line_width=3, line_dash="dash", line_color="red", annotation_text="Cold War")
     fig.add_vline(x=1949, line_width=3, line_dash="dash", line_color="green", annotation_text="1949")
-    fig.add_vline(x=1962, line_width=3, line_dash="dash", line_color="green", annotation_text="1949")
+    fig.add_vline(x=1962, line_width=3, line_dash="dash", line_color="green", annotation_text="1962")
     fig.add_vline(x=1963, line_width=3, line_dash="dash", line_color="green", annotation_text="1963")
     fig.add_vline(x=1969, line_width=3, line_dash="dash", line_color="green", annotation_text="1969")
     fig.add_vline(x=1979, line_width=3, line_dash="dash", line_color="green", annotation_text="1979")
