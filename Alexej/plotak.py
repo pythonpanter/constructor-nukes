32846c5ef064fbc67e6dac8a767d8b2971f05d47
Alexej Khalilzada <alexej.khalilzada@googlemail.com>
2022-12-02 16:56:42 +0100

    fix annotations

--- Alexej/plotak.py
+++ Alexej/plotak.py
@@ -80,14 +80,14 @@ def dummy(dataframe):
     
     # mark special events
     # fig.add_vline(x=1982, line_width=3, line_dash="dash", line_color="green", annotation_text="ThisIsAtestText")
-    fig.add_vrect(x0=1947, x1=1991, line_width=3, line_dash="dash", line_color="red")
-    fig.add_vline(x=1949, line_width=3, line_dash="dash", line_color="green")
-    fig.add_vline(x=1962, line_width=3, line_dash="dash", line_color="green")
-    fig.add_vline(x=1963, line_width=3, line_dash="dash", line_color="green")
-    fig.add_vline(x=1969, line_width=3, line_dash="dash", line_color="green")
-    fig.add_vline(x=1979, line_width=3, line_dash="dash", line_color="green")
-    fig.add_vline(x=1998, line_width=3, line_dash="dash", line_color="green")
-    fig.add_vline(x=2006, line_width=3, line_dash="dash", line_color="green")
+    fig.add_vrect(x0=1947, x1=1991, line_width=3, line_dash="dash", line_color="red", annotation_text="Cold War")
+    fig.add_vline(x=1949, line_width=3, line_dash="dash", line_color="green", annotation_text="1949")
+    fig.add_vline(x=1962, line_width=3, line_dash="dash", line_color="green", annotation_text="1949")
+    fig.add_vline(x=1963, line_width=3, line_dash="dash", line_color="green", annotation_text="1963")
+    fig.add_vline(x=1969, line_width=3, line_dash="dash", line_color="green", annotation_text="1969")
+    fig.add_vline(x=1979, line_width=3, line_dash="dash", line_color="green", annotation_text="1979")
+    fig.add_vline(x=1998, line_width=3, line_dash="dash", line_color="green", annotation_text="1998")
+    fig.add_vline(x=2006, line_width=3, line_dash="dash", line_color="green", annotation_text="2006")
 
     # return figure
     return fig
