985ec9b31faba31f11b699fa887c3227307bec70
Alexej Khalilzada <alexej.khalilzada@googlemail.com>
2022-12-02 15:57:29 +0100

    fix annotations

--- Alexej/plotak.py
+++ Alexej/plotak.py
@@ -80,14 +80,8 @@ def dummy(dataframe):
     
     # mark special events
     # fig.add_vline(x=1982, line_width=3, line_dash="dash", line_color="green", annotation_text="ThisIsAtestText")
-    fig.add_vrect(x0=1947, x1=1991, line_width=3, line_dash="solid", line_color="red", annotation_text='The Cold War is a term commonly used to refer to a period \
-                                                                                                        of geopolitical tension between the United States and the \
-                                                                                                        Soviet Union and their respective allies, the Western Bloc \
-                                                                                                        and the Eastern Bloc. Historians do not fully agree on its \
-                                                                                                        starting and ending points, but the period is generally \
-                                                                                                        considered to span from the announcement of the Truman Doctrine \
-                                                                                                        on 12 March 1947 to the dissolution of the Soviet Union on 26 December 1991.')
-    fig.add_vline(x=1949, line_width=3, line_dash="dash", line_color="red", annotation_text="the Soviet Union secretly conducted its first successful weapon test (First Lightning, based on the American 'Fat Man' design) at the Semipalatinsk-21 in Kazakhstan. => ending the United States' monopoly on nuclear weapons.")
+    fig.add_vrect(x0=1947, x1=1991, line_width=3, line_dash="solid", line_color="green", annotation_text='The Cold War')
+    fig.add_vline(x=1949, line_width=3, line_dash="dash", line_color="red", annotation_text="Soviet Union secretly conducted its first successful weapon test (First Lightning, based on the American 'Fat Man' design) at the Semipalatinsk-21 in Kazakhstan. => ending the United States' monopoly on nuclear weapons.")
 
     # return figure
     return fig
