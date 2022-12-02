490886f6daf69341bf40f30e173979a86b2f1a39
Alexej Khalilzada <alexej.khalilzada@googlemail.com>
2022-12-02 16:23:41 +0100

    fix annotations

--- Alexej/plotak.py
+++ Alexej/plotak.py
@@ -80,16 +80,16 @@ def dummy(dataframe):
     
     # mark special events
     # fig.add_vline(x=1982, line_width=3, line_dash="dash", line_color="green", annotation_text="ThisIsAtestText")
-    fig.add_vrect(x0=1947, x1=1991, line_width=3, line_dash="solid", line_color="green", annotation_text='The Cold War')
-    fig.add_vline(x=1949, line_width=3, line_dash="dash", line_color="red", annotation_text="Soviet Union secretl first successful weapon test")
-    fig.add_vline(x=1962, line_width=3, line_dash="dot", line_color="yellow", annotation_text="The Cuban Missile Crisis")
-    fig.add_vline(x=1963, line_width=3, line_dash="longdash", line_color="blue", annotation_text="Improved US-USSR relations")
-    fig.add_vline(x=1963, line_width=3, line_dash="dashdot", line_color="grey", annotation_text="The Treaty on the Non-Proliferation of Nuclear Weapons")
-    fig.add_vline(x=1969, line_width=3, line_dash="longdashdot", line_color="green", annotation_text="Détente")
-    fig.add_vline(x=1979, line_width=3, line_dash="dash", line_color="red", annotation_text="Soviet invasion of Afghanistan")
-    fig.add_vline(x=1979, line_width=3, line_dash="dot", line_color="yelloow", annotation_text="Dissolution of the Soviet Union")
-    fig.add_vline(x=1998, line_width=3, line_dash="longdash", line_color="green", annotation_text="India test detonated five more nuclear weapons")
-    fig.add_vline(x=2006, line_width=3, line_dash="dashdot", line_color="red", annotation_text="North Korea announced first nuclear test")
+    fig.add_vrect(x0=1947, x1=1991, line_width=3, line_dash="solid", line_color="green")
+    fig.add_vline(x=1949, line_width=3, line_dash="dash", line_color="red")
+    fig.add_vline(x=1962, line_width=3, line_dash="dot", line_color="yellow")
+    fig.add_vline(x=1963, line_width=3, line_dash="longdash", line_color="blue")
+    fig.add_vline(x=1963, line_width=3, line_dash="dashdot", line_color="grey")
+    fig.add_vline(x=1969, line_width=3, line_dash="longdashdot", line_color="green")
+    fig.add_vline(x=1979, line_width=3, line_dash="dash", line_color="red")
+    fig.add_vline(x=1979, line_width=3, line_dash="dot", line_color="yelloow")
+    fig.add_vline(x=1998, line_width=3, line_dash="longdash", line_color="green")
+    fig.add_vline(x=2006, line_width=3, line_dash="dashdot", line_color="red")
 
     # return figure
     return fig
